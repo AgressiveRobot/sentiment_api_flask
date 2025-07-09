5c106852b60b91347ea8cae9d2fa2de92c43352a
Yuriy Georgievich L <57836342+AgressiveRobot@users.noreply.github.com>
2025-07-09 16:02:44 +0300

    Update db.py final

--- with_docker/src/db.py
+++ with_docker/src/db.py
@@ -1,28 +1,7 @@
 import sqlite3
 import os
-
-DB_PATH = 'reviews.db'
-
-def init_db():
-    # Создаем базу данных, если ее еще нет
-    if not os.path.exists(DB_PATH):
-        with sqlite3.connect(DB_PATH) as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS reviews (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    text TEXT NOT NULL,
-                    sentiment INTERGER NOT NULL,
-                    created_at DATETIME NOT NULL
-                )
-            ''')
-            conn.commit()
-
-import sqlite3
-import os
 from datetime import datetime
 
-DB_PATH = 'reviews.db'
 
 def init_db():
     # Создаем базу данных и таблицу, если их еще нет
